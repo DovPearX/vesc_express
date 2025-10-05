3a37c3e9a5e2a4162edb69ffd1669d2e14ded1ed
DovPear <103691298+DovPearX@users.noreply.github.com>
2025-10-05 14:00:28 +0200

    Move GNSS satellite count extension to hw_ecubox
    The 'gnss-n-sat' Lisp extension was relocated from lispif_vesc_extensions.c to hw_ecubox.c.

--- main/hwconf/hw_ecubox.c
+++ main/hwconf/hw_ecubox.c
@@ -1,6 +1,24 @@
 #include "hw_devkit_c3.h"
 #include "lispif.h"
 #include "lispbm.h"
+#include "nmea.h"
+#include "ublox.h"
+
+static lbm_value ext_gnss_n_sat(lbm_value *args, lbm_uint argn) {
+	(void)args; (void)argn;
+	return lbm_enc_i(nmea_get_state()->gga.n_sat);
+}
+
+static void load_extensions(bool main_found) {
+	if (main_found) {
+		return;
+	}
+
+	//memset(&syms_vesc, 0, sizeof(syms_vesc));
+
+	// Add GNSS sat number extension
+	lbm_add_extension("gnss-n-sat", ext_gnss_n_sat);
+}
 
 void hw_init(void) {
 	gpio_config_t io_conf = {};
@@ -12,4 +30,6 @@ void hw_init(void) {
 	gpio_config(&io_conf);
 
 	LED_BLUE_OFF();
+
+	lispif_add_ext_load_callback(load_extensions);
 }
